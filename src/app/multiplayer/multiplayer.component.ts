9f4e4a2850b5a64b0c677a512cb9a5a57cd992c2
Andrew <alvino.aj@gmail.com>
2017-02-13 10:38:52 +0000

    added functionality to multiplayer component

--- src/app/multiplayer/multiplayer.component.ts
+++ src/app/multiplayer/multiplayer.component.ts
@@ -1,4 +1,11 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Result } from '../shared/models/result.model';
+import { Player } from '../shared/models/player.model';
+import { AppState } from '../shared/store/';
+import { ADD_RESULT } from '../shared/store/results.action';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-multiplayer',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./multiplayer.component.sass']
 })
 export class MultiplayerComponent implements OnInit {
+  results$;
+  player1: Player;
+  player2: Player;
+  player1Visible: boolean = true;
+  player2Visible: boolean = true;
+  weaponsArray: string[] = [
+    'rock',
+    'paper',
+    'scissors'
+  ];
 
-  constructor() { }
+  constructor(
+    private _store: Store<AppState>
+  ) {
+  }
 
   ngOnInit() {
+  }
+
+  setPlayer1($event: Player) {
+    this.player1 = new Player($event.name, $event.weapon);
+    this.player2 = new Player('computer', this.weaponsArray[this.getRandomInt(1, 3)]);
+    this.player1Visible = false;
+  }
+
+  finishGame() {
+    const newResult = new Result(this.player1.name, this.player1.weapon, this.player2.name, this.player2.weapon);
+    this._store.dispatch({ type: ADD_RESULT, payload: newResult });
+    this.player1Visible = true;
+    this.player2Visible = true;
+  }
+
+  getRandomInt(min, max) {
+    //Will return a number inside the given range, inclusive of both minimum and maximum
+    //i.e. if min=0, max=20, returns a number from 0-20
+    return (Math.floor(Math.random() * (max - min + 1)) + min) - 1;
   }
 
 }
