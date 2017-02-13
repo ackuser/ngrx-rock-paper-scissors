1cadeff293ab933924095a4168029aab94611e5a
Andrew <alvino.aj@gmail.com>
2017-02-13 10:35:37 +0000

    Added result list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RpsSelectionComponent } from './components/rps-selection/rps-selection.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [RpsSelectionComponent],
-  exports: [RpsSelectionComponent, FormsModule],
+  declarations: [RpsSelectionComponent, ResultListComponent],
+  exports: [RpsSelectionComponent, ResultListComponent, FormsModule],
 })
 export class SharedModule { }
